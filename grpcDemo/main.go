9adde58fe69333ea072394c7c557c21a993e3a9f
--add <ccssliu@163.com>
2021-11-30 20:25:44 +0800

    addprocfile

--- grpcDemo/main.go
+++ grpcDemo/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"goLibrary/grpcDemo/proto/product"
-	"service"
-	"google.golang.org/grpc"
-	"net"
-)
-
 func main() {
-	// 启动 rpc service
-	go func() {
-		rpcServer := grpc.NewServer()
-		// 注册 product server gen 自动生成的
-		product.RegisterProductServerServer(rpcServer, new(servicew.ProductServer))
-		//
-		listener, err := net.Listen("tcp", ":9001")
-		if err != nil {
-			panic(err)
-		}
-		_ = rpcServer.Serve(listener)
-	}()
-
-	conn, err := grpc.Dial(":9001", grpc.WithInsecure())
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = conn.Close() }()
-
-	cli := product.NewProductServerClient(conn)
-	res, err := cli.GetProduct(context.Background(), &product.ProductReq{Id: 1})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("hello", res.Name)
 
 }
